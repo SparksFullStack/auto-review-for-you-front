eb31a7ceb9ee3734ab04ef2358bba7abcde669bd
SparksFullStack <csparks@antioch.edu>
2018-11-18 15:16:15 -0800

    updated navbar--still needs work

--- src/components/MainPage/navbar.js
+++ src/components/MainPage/navbar.js
@@ -3,6 +3,19 @@ import AuthService from '../Auth/authservice';
 import './navbar.css';
 import Media from 'react-media';
 import HamburgerMenu from './hamburgermenu';
+import {
+  Navbar,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Collapse,
+  NavbarToggler,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
 
 const Auth = new AuthService();
 
@@ -11,10 +24,11 @@ const styles = {
     display: 'flex',
     justifyContent: 'flex-end',
     marginBottom: 20,
+    padding: '40px'
   }
 }
 
-class Navbar extends Component {
+class MainNavbar extends Component {
   constructor(props) {
     super(props);
     this.state = { 
@@ -22,6 +36,9 @@ class Navbar extends Component {
         isOpen: false,
         type: 'login'
       },
+      navState: {
+        isOpen: false,
+      }
 
      }
   }
@@ -30,50 +47,87 @@ class Navbar extends Component {
     Auth.logout();
   };
 
+  navToggler = () => {
+    this.setState({ navState: { isOpen: !this.state.navState.isOpen }});
+  }
+
+  handleRenderNavItems = () => {
+    if (this.props.isLoggedIn){
+      return (
+        <Navbar color="primary" expand="lg" light>
+           <NavbarBrand className="text-white" href="/">auto review for you</NavbarBrand>
+            <Nav className="ml-auto" navbar>
+              <NavItem >
+                <NavLink className="text-white" href="/">
+                  <i className="fas fa-home"></i> Home
+                </NavLink>
+              </NavItem>
+
+              <NavItem >
+                <NavLink className="text-white" href="/MyReviews">
+                  <i className="fas fa-newspaper"></i> My Reviews
+                </NavLink>
+              </NavItem>
+
+              <NavItem >
+                <NavLink className="text-white" href="/Billing">
+                  <i className="fas fa-money-bill-alt"></i> Billing
+                </NavLink>
+              </NavItem>
+
+              <NavItem >
+                <NavLink className="text-white" href="/Settings">
+                  <i className="fas fa-cogs"></i> Settings
+                </NavLink>
+              </NavItem>
+
+              <NavItem >
+                <NavLink className="text-white" href="/login">
+                <i className="fas fa-sign-out-alt"></i> Sign Out
+                </NavLink>
+              </NavItem>
+            </Nav>
+          
+        </Navbar>
+      )
+    } else return (
+      <Navbar color="primary" expand="md" light>
+        <Nav className="ml-auto" navbar>
+          <NavItem >
+            <NavLink className="text-white" href="/">
+              <i className="fas fa-home"></i> Home
+            </NavLink>
+          </NavItem>
+
+          <NavItem >
+            <NavLink className="text-white" href="/login">
+              <i className="fas fa-sign-in-alt"></i> Sign In
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+    )
+  }
 
   render() { 
     return (
-      <div className="navbar-container">
-          <Media query='(max-width: 550px)'>
+      <div>
+          {/* <Media query='(max-width: 1000px)'>
             {matches => 
               matches ? 
-              <div style={styles.hamburgerStyles} id='hamburger-menu'>
+              <div style={{backgroundColor: "blue"}}>
+                <div style={styles.hamburgerStyles} id='hamburger-menu'>
                 <HamburgerMenu right isLoggedIn={this.props.isLoggedIn} /> 
               </div>
-              :
-              <div className="nc">
-                <a className="navbar-item" href="/">
-                  Home
-                </a>
-          
-                {this.props.isLoggedIn ?
-                <a className="navbar-item" href="/MyReviews">
-                  My Reviews
-                </a> : <Fragment />
-                }
-
-                {this.props.isLoggedIn ?
-                <a className="navbar-item" href="/Billing">
-                  Billing
-                </a> : <Fragment />
-                }
-
-                {this.props.isLoggedIn ?
-                <a className="navbar-item" href="/Settings">
-                  Settings
-                </a> : <Fragment />
-                }
-
-                {!this.props.isLoggedIn ?
-                  <a className='navbar-item' href="/login">Sign In</a> :
-                  <a className="navbar-item" href="/" onClick={this.signOut}>Sign Out</a>
-                }
-              </div>
+              : <Fragment>{this.handleRenderNavItems()}</Fragment>
             }
-          </Media>
+          </Media> */}
+
+          {this.handleRenderNavItems()}
+
       </div>
     );
   }
 }
  
-export default Navbar;
+export default MainNavbar;
